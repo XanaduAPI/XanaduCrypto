db98a17719d69e9ed047a3232c81d1b43ea1d622
怯花颜 <45027459+XanaduAPI@users.noreply.github.com>
2021-06-08 17:48:53 +0800

    增加dllmain

---
+++ source/dllmain.cpp
@@ -0,0 +1,46 @@
+﻿#include <XanaduCrypto/Xanadu.h>
+
+bool XANADUAPI Xanadu_Crypto_Initialize() noexcept
+{
+	return true;
+};
+
+void XANADUAPI Xanadu_Crypto_Release() noexcept
+{
+};
+
+
+#ifdef XANADU_SYSTEM_WINDOWS
+extern "C" BOOL WINAPI DllMain(HANDLE _HDllHandle, DWORD _Reason, LPVOID _Reserved)
+{
+	XANADU_UNPARAMETER(_HDllHandle);
+	XANADU_UNPARAMETER(_Reserved);
+
+	switch(_Reason)
+	{
+		case DLL_PROCESS_ATTACH:
+			Xanadu_Crypto_Initialize();
+			break;
+		case DLL_THREAD_ATTACH:
+			break;
+		case DLL_THREAD_DETACH:
+			break;
+		case DLL_PROCESS_DETACH:
+			Xanadu_Crypto_Release();
+			break;
+		default:
+			break;
+	}
+	return TRUE;
+}
+#else
+__attribute((constructor)) void _Xanadu_Crypto_Dynamic_Library_Init()
+{
+	Xanadu_Crypto_Initialize();
+};
+
+__attribute((destructor)) void _Xanadu_Crypto_Dynamic_Library_Fini()
+{
+	Xanadu_Crypto_Release();
+};
+#endif
